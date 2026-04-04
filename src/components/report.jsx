056995fab8ab07f8ddd0adb8bdd41c868eb757c1
JanCesenek <jan.cesenekCZ@gmail.com>
2026-04-04 13:52:03 +0200

    added deletion of users/data

--- src/components/report.jsx
+++ src/components/report.jsx
@@ -1,8 +1,9 @@
-import { MdDateRange } from "react-icons/md";
+import { ImEnter } from "react-icons/im";
+import { MdDateRange, MdDeleteForever } from "react-icons/md";
 import { FaHourglassHalf } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 
-const Report = ({ start, end, lat, lon }) => {
+const Report = ({ start, end, lat, lon, open, remove }) => {
   return (
     <div className="flex flex-col p-5 rounded-md bg-blue-800/60 shadow-lg shadow-800/50 text-[2rem]">
       <div className="flex items-center [&>*]:mx-2">
@@ -16,6 +17,13 @@ const Report = ({ start, end, lat, lon }) => {
       <div className="flex items-center [&>*]:mx-2">
         <FaLocationDot />
         <span>{`${lat}, ${lon}`}</span>
+      </div>
+      <div className="flex justify-between items-center [&>*]:mx-2 mt-10">
+        <MdDeleteForever
+          className="hover:cursor-pointer hover:scale-125 text-red-600"
+          onClick={remove}
+        />
+        <ImEnter className="hover:cursor-pointer hover:scale-125" onClick={open} />
       </div>
     </div>
   );
--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext, useEffect } from "react";
 import { useUpdate } from "../hooks/use-update";
 import { AuthContext } from "../context/AuthContext";
+import { api } from "../core/api";
 import Login from "../components/login";
 import SignUp from "../components/signup";
 import Button from "../components/button";
@@ -23,7 +24,50 @@ const Auth = () => {
   const [submitting, setSubmitting] = useState(false);
   const [detail, setDetail] = useState(false);
 
-  const { curUser, logOut } = useContext(AuthContext);
+  const { curUser, logOut, notifyContext } = useContext(AuthContext);
+  const getUser = uData?.find((user) => user.username === curUser);
+  const userID = getUser?.id;
+
+  const deleteReport = async (id) => {
+    if (window.confirm("Are you sure you want to delete this report?")) {
+      setSubmitting(true);
+      await api
+        .delete(`/data/${id}`)
+        .then(async () => {
+          await dRefetch();
+          notifyContext("Report deleted successfully", "success");
+        })
+        .catch((err) => {
+          console.log(`Delete req - ${err}`);
+          notifyContext("Failed to delete report", "error");
+        })
+        .finally(() => setSubmitting(false));
+    }
+  };
+
+  const deleteAccount = async () => {
+    if (window.confirm("Are you sure you want to delete your account?")) {
+      setSubmitting(true);
+      await api
+        .delete(`/users/${userID}`)
+        .then(async () => {
+          await uRefetch();
+          logOut();
+          notifyContext("Account deleted successfully", "success");
+        })
+        .catch((err) => {
+          console.log(`Delete req - ${err}`);
+          notifyContext("Failed to delete account", "error");
+        })
+        .finally(() => setSubmitting(false));
+    }
+  };
+
+  const loading = uLoading || dLoading;
+
+  if (loading) {
+    return <div>Loading data...</div>;
+  }
 
   return (
     <div
@@ -34,20 +78,29 @@ const Auth = () => {
         <div className="w-full flex flex-col items-center [&>*]:my-5 my-10">
           <div className="text-[2rem]">{curUser} logged in</div>
           <Button title="Log Out" click={logOut} />
+          <Button title="Delete Account" click={deleteAccount} />
           {detail ? (
             <ReportDetail data={detail?.data?.data?.timelines[0]} exit={() => setDetail(false)} />
           ) : (
             dData?.map((el) => {
-              const start = new Date(el?.data?.data?.timelines[0]?.startTime).toLocaleDateString();
-              const end = new Date(el?.data?.data?.timelines[0]?.endTime).toLocaleDateString();
-              return (
-                <div
-                  key={el.id}
-                  onClick={() => setDetail(el)}
-                  className="hover:cursor-pointer hover:scale-110">
-                  <Report start={start} end={end} lat={el.lat} lon={el.lon} />
-                </div>
-              );
+              if (el.userID === userID) {
+                const start = new Date(
+                  el?.data?.data?.timelines[0]?.startTime,
+                ).toLocaleDateString();
+                const end = new Date(el?.data?.data?.timelines[0]?.endTime).toLocaleDateString();
+                return (
+                  <div key={el.id}>
+                    <Report
+                      start={start}
+                      end={end}
+                      lat={el.lat}
+                      lon={el.lon}
+                      open={() => setDetail(el)}
+                      remove={() => deleteReport(el.id)}
+                    />
+                  </div>
+                );
+              }
             })
           )}
         </div>
